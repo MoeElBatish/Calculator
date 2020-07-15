3e91c6e99d449bfb307d13c22f59a583b6aac8c1
Moe Elbatish <moebatish@gmail.com>
2020-07-14 23:08:53 -0400

    Added format and evaluate to handle user input

--- scripts/main.js
+++ scripts/main.js
@@ -11,5 +11,14 @@ const operations = {
     "-": (a,b) => {
         return a - b;
     }
-
+}
+const format = (exp) => {
+    let arr = exp.split(" ");
+    arr[0] = Number(arr[0]);
+    arr[2] = Number(arr[2]);
+    return arr;
+}
+const evaluate = (arr) => {
+    let answer = operations[arr[1]](arr[0],arr[2])
+    return answer;
 }
