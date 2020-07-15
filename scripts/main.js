bef9ee7353685988ca83c2c48ae79c383cb00ffa
Moe Elbatish <moebatish@gmail.com>
2020-07-14 23:00:26 -0400

    Added basic operations

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+const operations = {
+    "+": (a,b) => {
+        return a + b;
+    },
+    "x": (a,b) => {
+        return a * b;
+    },
+    "/": (a,b) => {
+        return Math.floor(a / b);
+    },
+    "-": (a,b) => {
+        return a - b;
+    }
+
+}
